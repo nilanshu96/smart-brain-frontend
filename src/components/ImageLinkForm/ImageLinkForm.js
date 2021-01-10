859b75b089db45d5ff52b0995ccda128c949f231
Nilanshu V Rajmane <rajmanenilanshu@gmail.com>
2021-01-10 15:56:01 +0530

    created the fronted ui for the app

---
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -0,0 +1,21 @@
+import './ImageLinkForm.css';
+
+const ImageLinkForm = () => {
+  return (
+    <div>
+      <div className="f3">
+        <p>This button can detect human faces in your pictures. Please give it a try!</p>
+      </div>
+      <div className="center">
+        <div className="form pa4 br3 shadow-5 center">
+          <input type="text" className="f4 pa2 w-70 center" />
+          <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple">
+            Detect
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ImageLinkForm;
